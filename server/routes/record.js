244764f5dafff85eac6cdada7d420d55b002d502
lykkell <lykkellit@gmail.com>
2022-07-11 14:28:49 +0200

    add collection Db users

--- server/routes/record.js
+++ server/routes/record.js
@@ -14,9 +14,9 @@ const ObjectId = require("mongodb").ObjectId;
  
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
- let db_connect = dbo.getDb("employees");
+ let db_connect = dbo.getDb("dor");
  db_connect
-   .collection("records")
+   .collection("users")
    .find({})
    .toArray(function (err, result) {
      if (err) throw err;
@@ -29,7 +29,7 @@ recordRoutes.route("/record/:id").get(function (req, res) {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId( req.params.id )};
  db_connect
-     .collection("records")
+     .collection("users")
      .findOne(myquery, function (err, result) {
        if (err) throw err;
        res.json(result);
@@ -51,7 +51,7 @@ recordRoutes.route("/record/add").post(function (req, response) {
     user_login: req.body.user_login,
     user_pass: req.body.user_pass,
  };
- db_connect.collection("records").insertOne(myobj, function (err, res) {
+ db_connect.collection("users").insertOne(myobj, function (err, res) {
    if (err) throw err;
    response.json(res);
  });
@@ -81,7 +81,7 @@ recordRoutes.route("/update/:id").post(function (req, response) {
 recordRoutes.route("/:id").delete((req, response) => {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId( req.params.id )};
- db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+ db_connect.collection("users").deleteOne(myquery, function (err, obj) {
    if (err) throw err;
    console.log("1 document deleted");
    response.json(obj);
