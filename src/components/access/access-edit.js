28ff7ecef2272ad57ae76170fb27c775f8d99cc4
lykkell <lykkellit@gmail.com>
2022-08-07 19:34:55 +0200

    updated  page Access data

--- src/components/access/access-edit.js
+++ src/components/access/access-edit.js
@@ -86,189 +86,158 @@ export default function AccessEdit() {
  // This following section will display the form that takes input from the user to update the data.
  return (
    <div>
-     <h3>Update Access</h3>
-     <form onSubmit={onSubmit}>
-     <div className="form-group">
-         <label htmlFor="user_name">user_name: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="user_name"
-           value={form.user_name}
-           onChange={(e) => updateForm({ user_name: e.target.value })}
-         />
-       </div>
-     <div className="form-group">
-         <label htmlFor="user_login">user_login: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="user_login"
-           value={form.user_login}
-           onChange={(e) => updateForm({ user_login: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="user_pass">user_pass: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="user_pass"
-           value={form.user_pass}
-           onChange={(e) => updateForm({ user_pass: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="user_status">user_status: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="user_status"
-           value={form.user_status}
-           onChange={(e) => updateForm({ user_status: e.target.value })}
-         />
-       </div>
-       {/* <div className="form-group">
-         <label htmlFor="user_update">user_update: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="user_update"
-           value={form.user_update}
-           onChange={(e) => updateForm({ user_update: e.target.value })}
-         />
-       </div> */}
-       <div className="form-group">
-        <label htmlFor="positionstart" className="form-check-label">Access group:
-        <div className="form-check form-check-inline">
+        <form onSubmit={onSubmit}>Access data:
+        <div className="form-group">
+            <label htmlFor="user_name">user_name: </label>
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positionuser"
-                value="user"
-                checked={form.access_group === "user"}
-                onChange={(e) => updateForm({access_group: e.target.value })}
+            type="text"
+            className="form-control"
+            id="user_name"
+            value={form.user_name}
+            onChange={(e) => updateForm({ user_name: e.target.value })}
             />
-            <label htmlFor="positionuser" className="form-check-label">user</label>
         </div>
-        <div className="form-check form-check-inline">
+        <div className="form-group">
+            <label htmlFor="user_login">user_login: </label>
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positionadmin"
-                value="admin"
-                checked={form.access_group === "admin"}
-                onChange={(e) => updateForm({access_group: e.target.value })}
+            type="text"
+            className="form-control"
+            id="user_login"
+            value={form.user_login}
+            onChange={(e) => updateForm({ user_login: e.target.value })}
             />
-            <label htmlFor="positioadmin" className="form-check-label">admin</label>
         </div>
-        <div className="form-check form-check-inline">
+        <div className="form-group">
+            <label htmlFor="user_pass">user_pass: </label>
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positiondevelopment"
-                value="development"
-                checked={form.access_group === "development"}
-                onChange={(e) => updateForm({access_group: e.target.value })}
+            type="text"
+            className="form-control"
+            id="user_pass"
+            value={form.user_pass}
+            onChange={(e) => updateForm({ user_pass: e.target.value })}
             />
-            <label htmlFor="positiondevelopment" className="form-check-label">development</label>
         </div>
-        </label>
-        
-       </div>
-       <div className="form-group">
-        <label htmlFor="positionstart" className="form-check-label">Access level:
-        <div className="form-check form-check-inline">
+        <div className="form-group">
+            <label htmlFor="user_status">user_status: </label>
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positionstart"
-                value="start"
-                checked={form.access_level === "start"}
-                onChange={(e) => updateForm({access_level: e.target.value })}
+            type="text"
+            className="form-control"
+            id="user_status"
+            value={form.user_status}
+            onChange={(e) => updateForm({ user_status: e.target.value })}
             />
-            <label htmlFor="positionstart" className="form-check-label">start</label>
         </div>
-        <div className="form-check form-check-inline">
+        <div className="form-group">
+            <label htmlFor="user_update">user_update: </label>
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positionstandart"
-                value="standart"
-                checked={form.access_level === "standart"}
-                onChange={(e) => updateForm({access_level: e.target.value })}
+            type="text"
+            className="form-control"
+            id="user_update"
+            value={form.user_update}
+            onChange={(e) => updateForm({ user_update: e.target.value })}
             />
-            <label htmlFor="positionstandart" className="form-check-label">standart</label>
         </div>
-        <div className="form-check form-check-inline">
+        </form>
+        <br 
+       />
+        <form onSubmit={onSubmit}>
+        <div className="form-group">
+            <label htmlFor="positionstart" className="form-check-label">Access group:
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positionuser"
+                    value="user"
+                    checked={form.access_group === "user"}
+                    onChange={(e) => updateForm({access_group: e.target.value })}
+                />
+                <label htmlFor="positionuser" className="form-check-label">user</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positionadmin"
+                    value="admin"
+                    checked={form.access_group === "admin"}
+                    onChange={(e) => updateForm({access_group: e.target.value })}
+                />
+                <label htmlFor="positioadmin" className="form-check-label">admin</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positiondevelopment"
+                    value="development"
+                    checked={form.access_group === "development"}
+                    onChange={(e) => updateForm({access_group: e.target.value })}
+                />
+                <label htmlFor="positiondevelopment" className="form-check-label">development</label>
+            </div>
+            </label>
+        </div>
+        </form>
+       <br 
+       />
+        <form onSubmit={onSubmit}>
+        <div className="form-group">
+            <label htmlFor="positionstart" className="form-check-label">Access level:
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positionstart"
+                    value="start"
+                    checked={form.access_level === "start"}
+                    onChange={(e) => updateForm({access_level: e.target.value })}
+                />
+                <label htmlFor="positionstart" className="form-check-label">start</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positionstandart"
+                    value="standart"
+                    checked={form.access_level === "standart"}
+                    onChange={(e) => updateForm({access_level: e.target.value })}
+                />
+                <label htmlFor="positionstandart" className="form-check-label">standart</label>
+            </div>
+            <div className="form-check form-check-inline">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="positionOptions"
+                    id="positionadvanced"
+                    value="advanced"
+                    checked={form.access_level === "advanced"}
+                    onChange={(e) => updateForm({access_level: e.target.value })}
+                />
+                <label htmlFor="positionadvanced" className="form-check-label">advanced</label>
+            </div>
+            </label>
+        </div>
+        </form>
+        <br 
+       />
+        <form onSubmit={onSubmit}>
+        <div className="form-group">
             <input
-                className="form-check-input"
-                type="radio"
-                name="positionOptions"
-                id="positionadvanced"
-                value="advanced"
-                checked={form.access_level === "advanced"}
-                onChange={(e) => updateForm({access_level: e.target.value })}
+            type="submit"
+            value="Update"
+            className="btn btn-primary"
             />
-            <label htmlFor="positionadvanced" className="form-check-label">advanced</label>
         </div>
-        </label>
-       </div>
-      
-       {/* <div className="form-group"> */}
-         {/* <div className="form-check form-check-inline">
-           <input
-             className="form-check-input"
-             type="radio"
-             name="positionOptions"
-             id="positionIntern"
-             value="Intern"
-             checked={form.level === "Intern"}
-             onChange={(e) => updateForm({ level: e.target.value })}
-           />
-           <label htmlFor="positionIntern" className="form-check-label">Intern</label>
-         </div> */}
-         {/* <div className="form-check form-check-inline">
-           <input
-             className="form-check-input"
-             type="radio"
-             name="positionOptions"
-             id="positionJunior"
-             value="Junior"
-             checked={form.level === "Junior"}
-             onChange={(e) => updateForm({ level: e.target.value })}
-           />
-           <label htmlFor="positionJunior" className="form-check-label">Junior</label>
-         </div> */}
-         {/* <div className="form-check form-check-inline">
-           <input
-             className="form-check-input"
-             type="radio"
-             name="positionOptions"
-             id="positionSenior"
-             value="Senior"
-             checked={form.level === "Senior"}
-             onChange={(e) => updateForm({ level: e.target.value })}
-           />
-           <label htmlFor="positionSenior" className="form-check-label">Senior</label>
-        </div> */}
-       {/* </div> */}
-       <br />
- 
-       <div className="form-group">
-         <input
-           type="submit"
-           value="Update Access"
-           className="btn btn-primary"
-         />
-         <Link to='/' className="btn btn-primary">{t('Dashboard')}</Link>
-       </div>
-     </form>
+        <Link to='/' className="btn btn-primary">{t('Dashboard')}</Link>
+        </form>
    </div>
  );
 }
