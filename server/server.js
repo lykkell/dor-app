a1cc4a32605eea81330cb9960b76d598344de544
lykkell <lykkellit@gmail.com>
2022-11-20 12:33:32 +0200

    npm init mongoose

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const app = express();
+
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/user"));
@@ -11,12 +15,16 @@ app.use(require("./routes/certificate"));
 // get driver connection
 const dbo = require("./db/conn");
  
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      // perform a database connection when server starts
+      dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+      });
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (e) { console.log(e); }
+};
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+start ();
