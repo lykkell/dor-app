5d0081ebd83222a04c9610daebff9c404e403877
lykkell <lykkellit@gmail.com>
2022-11-21 10:26:32 +0200

    routing finished

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require ('./routes/index');
 
 require("dotenv").config({ path: "./config.env" });
 
