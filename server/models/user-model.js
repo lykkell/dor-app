632842c7f396a77be26b1ae162add46ff9e30c88
lykkell <lykkellit@gmail.com>
2022-11-21 10:14:07 +0200

    add user-model token-model

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -3,7 +3,8 @@ const {Schema, model} = require ('mongoose');
 const UserSchema = new Schema ({
     email: {type:String, unique: true, required: true},
     password: {type:String, required: true},
-    isActivated: {type:String, required: true},
-    
-
+    isActivated: {type:Boolean, required: false},
+    activationLink: {type:String},
 });
+
+module.exports = model('User', UserSchema);
