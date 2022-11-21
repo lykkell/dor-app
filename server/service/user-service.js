31952f68eaedd14c74c5f4d5d3f357cab97a9809
lykkell <lykkellit@gmail.com>
2022-11-21 12:39:11 +0200

    add to UserService f registration

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,5 +1,5 @@
 const UserModel = require('../models/user-model');
-const uuid = require('uuid');
+const uuid = require('uuid'); // it will generating random string
 const bcrypt = require('bcrypt'); //it will us hashPassword
 
 class UserService {
@@ -7,10 +7,11 @@ class UserService {
     async registration (email, password) {
         const newuser = await UserModel.findOne({email});
         if (newuser) {
-            throw new Error('There is user with email: ${email}');
+            throw new Error(`There is user with email: ${email}`);
         }
         const hashPassword = await bcrypt(password, 3);
-        const user = await UserModel.create({email, password: hashPassword});
+        const activationLink = await uuid.v4(); // genereting random string
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
     }
 }
 module.exports = new UserService();
