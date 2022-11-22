e3fe3dd0e9f7639beffb7753cf334d51be4bde1e
lykkell <lykkellit@gmail.com>
2022-11-22 22:17:30 +0200

    add MailService

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,6 +1,32 @@
-class MailService {
-    async SendActivationMail(to, link) {
+const nodemailer = require('nodemailer');
 
+class MailService {
+
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host:process.inv.SMTP_HOST, //switch on IMAP protocol Google
+            port:process.inv.SMTP_PORT,
+            secure: false,
+            auth: {
+                user:process.inv.SMTP_USER,
+                password:process.inv.SMTP_PASSWORD
+            }
+        })
+    };
+
+    async SendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from:process.inv.SMTP_USER,
+            to,
+            subject:'Activation of account, site' + process.inv.API_URI,
+            text:'',
+            html:
+                <div>
+                    <h1>Go by link for activation your account</h1>
+                    <a href="${link}">${link}</a>     
+                </div>
+            ,
+        })
     };
 }
 module.exports = new MailService();
